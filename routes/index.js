2655dbf823b325dadd2e63084ec04bf65a6d72d8
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2019-05-02 11:47:26 -0700

    added bcrypt

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+import models from '../models/';
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Parked' });
+});
+
+router.post('/register', async (req, res) => {
+  req.body.password = Bcrypt.hashSync(req.body.password, 10);
+  let user = new models.User(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
 module.exports = router;
