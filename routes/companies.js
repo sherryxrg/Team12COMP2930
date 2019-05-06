c06aa6ce3a96aa414207e10071de950ef0c481a4
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2019-05-05 18:47:50 -0700

    read for lots and companies

--- routes/companies.js
+++ routes/companies.js
@@ -4,4 +4,12 @@ import models from '../models/';
 
 const Company = models.Company;
 
+//Find company
+router.get('/:id', async (req,res) => {
+  Company.findById(req.params.id, (err, company) => {
+    if (err) return next(err);
+    res.send(company);
+  });
+});
+
 module.exports = router;
--- routes/lots.js
+++ routes/lots.js
@@ -4,4 +4,12 @@ import models from '../models/';
 
 const Lot = models.Lot;
 
+//Find lot
+router.get('/:id', async (req,res) => {
+  Lot.findById(req.params.id, (err, lot) => {
+    if (err) return next(err);
+    res.send(lot);
+  });
+});
+
 module.exports = router;
