b0795f8fe234d4f46a06de248c97927be3b0ceba
Sherry Guo <sherryguo@sherrys-mbp.wifi.bcit.ca>
2019-05-07 13:53:54 -0700

    -

--- models/index.js
+++ models/index.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
 import User from './user';
 import Vehicle from './vehicle';
-<<<<<<< HEAD
 import Card from './card';
-=======
 import Receipt from './receipt';
 import Lot from './lot';
 import Company from './company';
->>>>>>> development
 
 const connectDb = () => {
   return mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
@@ -16,13 +13,10 @@ const connectDb = () => {
 const models = {
   User,
   Vehicle,
-<<<<<<< HEAD
   Card,
-=======
   Receipt,
   Lot,
   Company
->>>>>>> development
 };
 
 export { connectDb };
