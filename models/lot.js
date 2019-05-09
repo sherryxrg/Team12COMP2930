699144ba12418cf0fb9c6828d6f5a6b9c50cb000
john lim <johnlim0612@gmail.com>
2019-05-09 13:30:10 -0700

    rate cost

--- models/lot.js
+++ models/lot.js
@@ -20,8 +20,42 @@ const lotSchema = new mongoose.Schema({
     required: true,
     ref: 'Company'
   },
+  rates: {
+    daily: {
+      cost: Number, 
+    },
+    hourly: {
+      endtime: Date,
+      cost: Number,
+    }
+  }
 });
+
 
 const Lot = mongoose.model('Lot', lotSchema);
 
 export default Lot;
+
+
+function findCost(){ 
+  var startTime = new Date();
+  var endTime = new Date(this.lotSchema.rates.hourly.endtime);
+  var startHour = startTime.getHours();
+  var startMin = startTime.getMinutes();
+  var endHour = endTimeTime.getHours();
+  var endMin = endTime.getMinutes();
+  
+  var parkTime = (endHour * 60 + endMin) - (startHour * 60 + startHour);
+  var hourlyTotal = parkTime * this.lotSchema.rates.hourly.cost;
+  if (hourlyTotal < dailyCost){
+    return hourlyTotal;
+  } else {
+    return this.lotSchema.rates.daily.cost;
+  }
+  
+}
+
+
+
+
+
