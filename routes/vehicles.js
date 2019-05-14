3433f7b86664bff9dc13cad914b02895f465d5ce
Steven Kang <buzzbumble@Stevens-MBP.hitronhub.home>
2019-05-14 14:59:32 -0700

    Dashboard <-> New Vehicle routes

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -16,23 +16,14 @@ router.post('/', async (req, res) => {
     req.body.user = user._id;
     let vehicle = new models.Vehicle(req.body);
     let result = await vehicle.save();
-    res.redirect('/vehicles/success');
+    res.redirect('/dashboard');
   } else {
     res.send("Not logged in.");
   }
 });
 
-router.get('/', (req, res) => {
+router.get('/new', (req, res) => {
   res.render('new_vehicle', {title: 'Create a Vehicle'});
-});
-
-router.get('/success', async (req, res) => {
-  const vehicles = await Vehicle.find();
-  const newVehicle = vehicles[vehicles.length -1]
-  res.render('new_vehicle_added', {
-    title: 'Vehicle Registered',
-    newVehicle
-  });
 });
 
 router.get('/all', async (req, res) => {
